c6e17653ef9bb2f183492319de59869a28cd7837
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2014-09-02 14:10:47 +0200

    deleted the intrusive lambda notation and replaced it by poly1d again

--- src/test.py
+++ src/test.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 
 def main():
     koeffs = [.3, 1.2, .1, 7]
-    p = lambda x: koeffs[0] * x**3 + koeffs[1] * x**2 + koeffs[2] * x + koeffs[3]
+    p = np.poly1d(koeffs)
     x = np.linspace(-5, 5, 100)
     y = p(x) + 2 * np.random.randn(100) - 1
     # fit

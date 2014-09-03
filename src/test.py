492827e2fc5244c313af4d25b563ad0f69425249
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2014-09-03 08:49:02 +0200

    Test change for closing an issue.
    This is a place for a longer comment. Closes #1

--- src/test.py
+++ src/test.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 
 
 def main():
-    koeffs = [.3, 1.2, .1, 7]
+    koeffs = [0.3, 1.2, 0.1, 7]
     p = np.poly1d(koeffs)
     x = np.linspace(-5, 5, 100)
     y = p(x) + 2 * np.random.randn(100) - 1
